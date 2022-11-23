0e7e5223b981df83486f60c46bfa53620e52015d
Valeria <valeria.lukashevich@gmail.com>
2022-11-23 13:35:20 +0200

    DB commit

--- db/services_data.sql
+++ db/services_data.sql
@@ -36,3 +36,17 @@ INSERT INTO public.contact (id, first_name, last_name, phone, institution, user_
 INSERT INTO public.offer (id, user_id, name, description, price_per_student, students_min, students_max, city_id, address, phone, date_add, additional_info) VALUES (1, 1, 'Narva linnuse klastus', 'Narva ajalooga tutvustus', 8, 5, 15, 3, 'Peterburi mnt 2', 53459241, '2022-11-23', 'Ei soobi õpilastele rattastooliga');
 INSERT INTO public.offer (id, user_id, name, description, price_per_student, students_min, students_max, city_id, address, phone, date_add, additional_info) VALUES (2, 2, 'ERMi külastus', 'Eesti kultuuri ja ajalooga tutvumine', 10, 8, 25, 2, 'Muuseumi tee 2', 55412578, '2022-11-22', 'Sobib vaegnägijatele');
 INSERT INTO public.offer (id, user_id, name, description, price_per_student, students_min, students_max, city_id, address, phone, date_add, additional_info) VALUES (3, 3, 'Videomängude muuseumi külastus', 'Interaktiivne kogemus', 5, 3, 10, 1, 'Mere pst 1', 58459125, '2022-11-21', 'Ei soobi õpilastele epilepsiaga');
+
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (1, 1, 2);
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (2, 1, 4);
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (3, 2, 2);
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (4, 2, 4);
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (5, 3, 5);
+INSERT INTO public.offer_subject (id, offer_id, subject_id) VALUES (6, 3, 6);
+
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (1, 1, 8);
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (2, 1, 9);
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (3, 2, 10);
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (4, 2, 11);
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (5, 3, 3);
+INSERT INTO public.offer_grade (id, offer_id, grade_id) VALUES (6, 3, 4);
