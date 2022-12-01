1e8f9c624c7ebe18edca3f107b36f9e5def5d49d
Valeria <valeria.lukashevich@gmail.com>
2022-12-01 09:30:43 +0200

    commit

--- src/main/java/ee/service2school/domain/contact/ContactRepository.java
+++ src/main/java/ee/service2school/domain/contact/ContactRepository.java
@@ -8,9 +8,6 @@ import java.util.Optional;
 
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
-//    @Override
-//    Optional<Contact> findById(Integer contactId);
-
     @Query("select c from Contact c where c.user.id = ?1")
     Contact findByUserId(Integer userId);
 
