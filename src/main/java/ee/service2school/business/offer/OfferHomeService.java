3be507aead98368c3ae0cf254f8d0441e76fdae1
admin <vaher.kaupo@gmail.com>
2022-12-07 16:51:36 +0200

    Homeview

--- src/main/java/ee/service2school/business/offer/OfferHomeService.java
+++ src/main/java/ee/service2school/business/offer/OfferHomeService.java
@@ -6,11 +6,13 @@ import ee.service2school.domain.form.city.CityService;
 import ee.service2school.domain.form.grade.Grade;
 import ee.service2school.domain.form.grade.GradeDto;
 import ee.service2school.domain.form.grade.GradeService;
+import ee.service2school.domain.form.subject.Subject;
 import ee.service2school.domain.form.subject.SubjectDto;
 import ee.service2school.domain.form.subject.SubjectService;
 import ee.service2school.domain.grade.OfferGrade;
 import ee.service2school.domain.grade.OfferGradeService;
 import ee.service2school.domain.offer.*;
+import ee.service2school.domain.subject.OfferSubject;
 import ee.service2school.domain.subject.OfferSubjectService;
 import ee.service2school.domain.user.User;
 import ee.service2school.domain.user.UserService;
@@ -116,7 +118,14 @@ public class OfferHomeService {
         for (SubjectDto subjectDto : subjectDtos) {
             if (subjectDto.getIsSelected()) {
                 Integer subjectId = subjectDto.getSubjectId();
-                subjectService.findSubjectBySubjectId(subjectId);
+                Subject subject = subjectService.findSubjectBySubjectId(subjectId);
+
+                OfferSubject offerSubject = new OfferSubject();
+                offerSubject.setOffer(offer);
+                offerSubject.setSubject(subject);
+
+                offerSubjectService.addOfferSubject(offerSubject);
+
             }
         }
     }
--- src/main/java/ee/service2school/domain/form/subject/SubjectService.java
+++ src/main/java/ee/service2school/domain/form/subject/SubjectService.java
@@ -3,8 +3,6 @@ package ee.service2school.domain.form.subject;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import javax.security.auth.Subject;
-import java.util.Optional;
 
 @Service
 public class SubjectService {
@@ -14,11 +12,8 @@ public class SubjectService {
 
 
     public Subject findSubjectBySubjectId(Integer subjectId) {
-        Optional<ee.service2school.domain.form.subject.Subject> optionalSubject = subjectRepository.findById(subjectId);
-
-        ee.service2school.domain.form.subject.Subject subject = optionalSubject.get();
-
-        return null;
+        Subject subject = subjectRepository.findById(subjectId).get();
+        return subject;
     }
 
 }
